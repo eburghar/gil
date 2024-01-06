7bab954bce37feaf6386f118641965494f0202b7
Éric BURGHARD <eric.burghard@itsufficient.me>
2024-01-06 08:15:41 +0100

    feat: remove context argument and use a static variable instead

--- src/api/users/keys.rs
+++ src/api/users/keys.rs
@@ -1,7 +1,7 @@
 pub mod add;
 pub mod delete;
-pub mod keys;
+pub mod list;
 
 pub use self::add::AddKey;
 pub use self::delete::DeleteKey;
-pub use self::keys::ListKeys;
+pub use self::list::ListKeys;
--- src/cmd/keys.rs
+++ src/cmd/keys.rs
@@ -1,7 +1,7 @@
 use crate::{
 	api::users::keys::{AddKey, DeleteKey, ListKeys},
 	args::{self, KeyIdType, KeysCmd},
-	context::CliContext,
+	context::CONTEXT,
 	types::SshKey,
 };
 
@@ -10,7 +10,7 @@ use gitlab::api::{self, Query};
 use ssh_key::{HashAlg, PublicKey};
 use std::fs::read_to_string;
 
-pub fn cmd(context: &CliContext, args: &args::Keys) -> Result<()> {
+pub fn cmd(args: &args::Keys) -> Result<()> {
 	match &args.cmd {
 		KeysCmd::Add(args) => {
 			// read ssh key on disk
@@ -19,24 +19,20 @@ pub fn cmd(context: &CliContext, args: &args::Keys) -> Result<()> {
 			let fingerprint = ssh_key.fingerprint(HashAlg::Sha256);
 
 			// get title from args or key
-			let title = args
-				.title
-				.as_ref()
-				.map(String::as_str)
-				.unwrap_or_else(|| ssh_key.comment());
+			let title = args.title.as_deref().unwrap_or_else(|| ssh_key.comment());
 
 			// try to delete existing key with same fingerprint on overwrite mode
 			if args.overwrite {
-				if let Ok(key) = context.get_key(&KeyIdType::FingerPrint(fingerprint)) {
+				if let Ok(key) = CONTEXT.get_key(&KeyIdType::FingerPrint(fingerprint)) {
 					let endpoint = DeleteKey::builder().key_id(key.id.value()).build()?;
-					api::ignore(endpoint).query(&context.gitlab)?;
+					api::ignore(endpoint).query(&CONTEXT.gitlab)?;
 				}
 			}
 
 			// try to add the key
 			let endpoint = AddKey::builder().key(&key).title(title).build()?;
 			api::ignore(endpoint)
-				.query(&context.gitlab)
+				.query(&CONTEXT.gitlab)
 				.with_context(|| format!("Failed to add ssh key {}", &args.key))?;
 			println!(
 				"Key {} has been {}",
@@ -48,15 +44,15 @@ pub fn cmd(context: &CliContext, args: &args::Keys) -> Result<()> {
 				}
 			);
 
-			if context.open {
-				let url = if let Ok(key) = context.get_key(&KeyIdType::FingerPrint(fingerprint)) {
+			if CONTEXT.open {
+				let url = if let Ok(key) = CONTEXT.get_key(&KeyIdType::FingerPrint(fingerprint)) {
 					format!(
 						"https://{}/-/profile/keys/{}",
-						context.config.host.name,
+						CONTEXT.config.host.name,
 						key.id.value()
 					)
 				} else {
-					format!("https://{}/-/profile/keys", context.config.host.name)
+					format!("https://{}/-/profile/keys", CONTEXT.config.host.name)
 				};
 				let _ = open::that(url);
 			}
@@ -65,25 +61,25 @@ pub fn cmd(context: &CliContext, args: &args::Keys) -> Result<()> {
 		}
 
 		KeysCmd::List(args) => {
-			let user = context.get_user(args.user.as_ref())?;
+			let user = CONTEXT.get_user(args.user.as_ref())?;
 			let endpoint = ListKeys::builder().user(&user.username).build()?;
-			let keys: Vec<SshKey> = endpoint.query(&context.gitlab)?;
+			let keys: Vec<SshKey> = endpoint.query(&CONTEXT.gitlab)?;
 
-			if context.open {
+			if CONTEXT.open {
 				let _ = open::that(format!(
 					"https://{}/-/profile/keys",
-					context.config.host.name
+					CONTEXT.config.host.name
 				));
 			}
 
-			context.print_keys(&keys, &user)
+			CONTEXT.print_keys(&keys, &user)
 		}
 
 		KeysCmd::Delete(args) => {
-			let key = context.get_key(&args.id)?;
+			let key = CONTEXT.get_key(&args.id)?;
 			let endpoint = DeleteKey::builder().key_id(key.id.value()).build()?;
 			api::ignore(endpoint)
-				.query(&context.gitlab)
+				.query(&CONTEXT.gitlab)
 				.with_context(|| format!("Failed to delete key {}", args.id))?;
 			if let KeyIdType::Id(id) = args.id {
 				println!("Key {} deleted", id);
@@ -91,10 +87,10 @@ pub fn cmd(context: &CliContext, args: &args::Keys) -> Result<()> {
 				println!("Key {}({}) deleted", args.id, key.id.value());
 			}
 
-			if context.open {
+			if CONTEXT.open {
 				let _ = open::that(format!(
 					"https://{}/-/profile/keys",
-					context.config.host.name
+					CONTEXT.config.host.name
 				));
 			}
 
