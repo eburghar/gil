e2b79484078aede913c783a4a6ecbd738db5d8f9
Éric BURGHARD <eric.burghard@itsufficient.me>
2024-04-24 21:32:35 +0200

    feat: add project (un)archive

--- src/cmd/project.rs
+++ src/cmd/project.rs
@@ -1,15 +1,55 @@
-use crate::{args, context::CliContext};
+use crate::{
+	args::{self, ProjectArchive, ProjectCmd},
+	context::CliContext,
+};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+use gitlab::api::{
+	self,
+	projects::{ArchiveProject, UnarchiveProject},
+	Query,
+};
 
 pub fn cmd(args: &args::Project) -> Result<()> {
-	let project = CliContext::global().get_project(args.project.as_ref())?;
-	// get a reference (a tag or a branch)
-	let ref_ = CliContext::global().get_ref(args.ref_.as_deref(), &project)?;
+	match &args.cmd {
+		ProjectCmd::Info(iargs) => {
+			let project = CliContext::global().get_project(args.project.as_ref())?;
+			// get a reference (a tag or a branch)
+			let ref_ = CliContext::global().get_ref(iargs.ref_.as_deref(), &project)?;
 
-	CliContext::global().print_project(&project, &ref_)?;
-	if CliContext::global().open {
-		let _ = open::that(format!("{}/-/tree/{}", &project.web_url, &ref_));
+			CliContext::global().print_project(&project, &ref_)?;
+			if CliContext::global().open {
+				let _ = open::that(format!("{}/-/tree/{}", &project.web_url, &ref_));
+			}
+			Ok(())
+		}
+		ProjectCmd::Archive(_args) => {
+			let project = CliContext::global().get_project(args.project.as_ref())?;
+			let endpoint = ArchiveProject::builder()
+				.project(project.id.value())
+				.build()?;
+			api::ignore(endpoint)
+				.query(&CliContext::global().gitlab)
+				.with_context(|| format!("failed to archive project {}", project.name))?;
+			println!(
+				"project {}({}) has been archived",
+				&project.name, project.id
+			);
+			Ok(())
+		}
+		ProjectCmd::Unarchive(_args) => {
+			let project = CliContext::global().get_project(args.project.as_ref())?;
+			let endpoint = UnarchiveProject::builder()
+				.project(project.id.value())
+				.build()?;
+			api::ignore(endpoint)
+				.query(&CliContext::global().gitlab)
+				.with_context(|| format!("failed to unarchive project {}", project.name))?;
+			println!(
+				"project {}({}) has been unarchived",
+				&project.name, project.id
+			);
+			Ok(())
+		}
 	}
-	Ok(())
 }
